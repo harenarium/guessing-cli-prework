4ba8b99e34e6e3dfce6b24cc9328c118ee23c2c5
Caroline Lee <carolinel2610@gmail.com>
2018-01-21 00:01:15 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,4 +1,4 @@
-# Code your solution here!
+g# Code your solution here!
 
 def run_guessing_game
   puts "Guess a number between 1 and 6."
@@ -8,8 +8,10 @@ def run_guessing_game
     guess = input.to_i
     if guess == number
       puts "You guessed the correct number!"
+      break
     elsif guess > 0 && guess < 7
       puts "The computer guessed #{number}."
+      break
     end
   end
   puts "Goodbye!"

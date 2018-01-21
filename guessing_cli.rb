f021dfbaf25644d6e8e53d78dfc345a713893787
Caroline Lee <carolinel2610@gmail.com>
2018-01-21 00:05:08 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -6,9 +6,8 @@ def run_guessing_game
   input = gets.chomp
   while input !="exit"
     number = rand(1..6)
-    guess = input.to_i
     binding.pry
-    if guess == number
+    if input.to_i == number
       puts "You guessed the correct number!"
       break
     elsif guess > 0 && guess < 7

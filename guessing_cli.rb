b643aee8a2cf8f3a39b2338df6245578a13c8811
Caroline Lee <carolinel2610@gmail.com>
2018-01-21 00:02:59 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,4 +1,5 @@
 # Code your solution here!
+require "pry"
 
 def run_guessing_game
   puts "Guess a number between 1 and 6."
@@ -6,6 +7,7 @@ def run_guessing_game
   while input !="exit"
     number = rand(1..6)
     guess = input.to_i
+    binding.pry
     if guess == number
       puts "You guessed the correct number!"
       break
